4d572fa56fcc18487aac48fe550ce92ebe9b65b5
Allan Ramos <allangrds@gmail.com>
2024-05-06 21:54:04 -0300

    chore(brackeys-godot-beginner-tutorial): add die down effect

--- brackeys-godot-beginner-tutorial/scripts/killzone.gd
+++ brackeys-godot-beginner-tutorial/scripts/killzone.gd
@@ -2,8 +2,13 @@ extends Area2D
 
 @onready var timer = $Timer
 
+const SLOWMOTION_TIME = 0.5
+
 func _on_body_entered(body):
+	Engine.time_scale = SLOWMOTION_TIME
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 func _on_timer_timeout():
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
